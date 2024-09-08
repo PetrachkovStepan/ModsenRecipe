8c2f2b184f58107cf4e6a22a029ee089df3b5a41
PetrachkovStepan <158708945+PetrachkovStepan@users.noreply.github.com>
2024-09-08 17:38:22 +0300

    feat: added 404 page markup

--- src/components/Layout/ShowMoreButton/index.tsx
+++ src/components/Layout/ShowMoreButton/index.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 import * as Styled from "./styled";
+import {getAllDishes} from "@utils/http/RecipeAPI"
 
 
 export default function ShowMoreButton() {
+  const getMore = async () => {
+    await getAllDishes()
+  }
   return (
-  <Styled.Container>
+  <Styled.Container onClick = {getMore}>
     <Styled.Text>Show More</Styled.Text>
   </Styled.Container>  
   );
